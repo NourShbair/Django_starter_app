3998b4670a4c006958de4bc26691709566bcd5f5
Nour <nshbair@gmail.com>
2025-02-03 19:49:07 +0000

    Enable debug mode

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(!2sxz-kb1kjsqlsyt01v41_dwz*&o8j2)xpg_oxkif-n3tf)9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
